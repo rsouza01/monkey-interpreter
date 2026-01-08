eaeba1d92d2177ea950b35b662a011d84fcb3d81
Rodrigo de Souza <rsouza01@gmail.com>
2026-01-08 14:18:42 +0000

    Parser tests added

---
+++ src/monkey/parser/parser_test.go
@@ -0,0 +1,72 @@
+package parser
+
+import (
+	"monkey/src/monkey/ast"
+	"monkey/src/monkey/lexer"
+	"testing"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+		let x = 5;
+		let y = 10;
+		let foobar = 838383;
+		`
+
+	// The lexer generates tokens from the input
+	l := lexer.New(input)
+
+	// The parser generates an AST from the tokens
+	p := New(l)
+
+	// The root node of our AST is a Program node
+	program := p.ParseProgram()
+
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
+			len(program.Statements))
+	}
+
+	// This is a declaration of an anonymous struct,
+	// instantiation and initialization at the same time.
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		t.Logf("stmt: %s", stmt)
+
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
+		return false
+	}
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not *ast.LetStatement. got=%T", s)
+		return false
+	}
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+		return false
+	}
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("s.Name not '%s'. got=%s", name, letStmt.Name)
+		return false
+	}
+	return true
+}
